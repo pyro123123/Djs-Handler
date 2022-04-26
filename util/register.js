55af33e2cd70664554917e584547c839af83c61e
pyro <patriotleagooii@gmail.com>
2022-04-26 08:31:20 +0800

    Register Slash command

--- util/register.js
+++ util/register.js
@@ -1,10 +1,15 @@
 const { getFile,getDir } = require("./file")
 const path = require("path");
 
+const addSlash = (client,data) => {
+  client.application.commands.set(data)
+  console.log(`Register ${data.length} slash command `)
+}
+
 const register = (client,cdir,edir) => {
   
   const dir = getDir(cdir)
-  
+  const slashCmd = [];
   for(const file of dir ) {
     const getFiles = getFile(path.join(cdir,file))
     
@@ -13,9 +18,18 @@ const register = (client,cdir,edir) => {
     
     const name = getFiles.name ?? file.split(".")[0];
     
+    if(getFiles.slash === true) {
+      slashCmd.push(getFiles)
+      continue;
+    } else if(getFiles.slash == "both") {
+      slashCmd.push(getFiles)
+    }
+    
     client.command.set(name,getFiles)
     console.log(`Register Command ${name}`)
   }
+  
+  addSlash(client,slashCmd)
   
   const dirs = getDir(edir)
   
