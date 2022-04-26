24160eec2916679708caa64f49dc961ea5ec3513
pyro <patriotleagooii@gmail.com>
2022-04-26 08:46:02 +0800

    Handler

---
+++ Command.js
@@ -0,0 +1,11 @@
+const CmdProp = {
+  name: String,
+  info: String,
+  slash: Boolean,
+  test: Boolean,
+  id: Array,
+  option: Array,
+  run: Function
+}
+
+module.exports = { CmdProp }
--- index.js
+++ index.js
@@ -28,25 +28,40 @@ const handler = class Handler {
        throw new Error("Client must be instanceof Discord.js Client")
     }
    
+   if (this.token === "") {
+     throw new Error("Require discord bot token to login");
+   }
+   
+   this.client.login(this.token).then(() => {
+     register(this.client, this.cmdDir, this.eventDir);
+   })
+   
     this.client.command = new Collection();
     
     this.client.on("messageCreate",(msg) => {
      
-      const { author,channel,content} = msg
+      const { author,channel,content,guild } = msg
      
       if(author.bot || !content.startsWith(this.prefix.length)) {
         return;
       }
       
+      const argument = content.trim().split(" ")
+      const cmdName = argument.shift().slice(this.prefix.length)
+      
+      const cmd = this.client.command.get(cmdName);
+      
+      if(!cmd) return;
+      
+      if(cmd.test && !this.testGuild.includes(guild.id)) return channel.send({
+        content: "This command can be use in specific guild only"
+      })
+      
+      cmd.run(msg,argument)
+      
+      
     })
     
-    if(this.token === "") {
-      throw new Error("Require discord bot token to login");
-    }
-    
-    this.client.login(this.token).then(() => {
-      register(this.client,this.cmdDir,this.eventDir);
-    })
   }
   
 }
